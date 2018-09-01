68753c0d7571e28430a89a5f58af456ec79161c6
Yi Xiao on mac <jenny9006@gmail.com>
2018-09-02 05:23:12 +1000

    cleanup

--- chrome-bg-div/background.js
+++ chrome-bg-div/background.js
@@ -1,7 +1,4 @@
 var css_enabled = false;
-//const css_id = "bg-div";
-//const css_file = css_id + ".css";
-//const css_file = "https://yixiaoyx.github.io/css/bg-div.css";
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
     console.log("mesage received: "+message);
@@ -20,38 +17,3 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
     }
 });
 
-//chrome.tabs.onCreated.addListener(addCSS);
-//chrome.tabs.onUpdated.addListener(addCSS);
-
-/*
-function addCSS() {
-    //console.log("adding custom CSS");
-    if (css_enabled) {
-        //chrome.tabs.insertCSS({file: "bg-div.css", "allFrames": true}, function() {
-        //    console.log("css inserted");
-        //});
-        var cssNode = document.getElementById(css_id);
-        if (cssNode === null) {
-            var link = document.createElement("link");
-            //link.href = chrome.extension.getURL(css_file);
-            link.href = css_file;
-            link.id = css_id;
-            link.type = "text/css";
-            link.rel = "stylesheet";
-            var html_head = document.getElementsByTagName("head");
-            console.log(html_head);
-            html_head[0].appendChild(link);
-            console.log("custom stylesheet inserted: "+link.href);
-        }
-        
-    }
-}
-
-function removeCSS() {
-    if (!css_enabled) {
-        var cssNode = document.getElementById(css_id);
-        cssNode && cssNode.parentNode.removeChild(cssNode);
-        console.log("custom stylesheet removed: " + cssNode.href);
-    }
-}
-*/
